af0a64bc42c1ef04d95bf042bbb0edaf6a01e9cc
rome-bop <park.srome@gmail.com>
2016-04-06 19:33:52 -0700

    resolved config load

--- server.js
+++ server.js
@@ -15,66 +15,63 @@ http.listen(app.get('port'), function() {
   console.log('Server running on localhost:' + app.get('port'));
 });
 
-fs.readFile('config.json', function(err, data) {
-  if (err) throw err;
-  else {
-    var config = JSON.parse(data);
-    var username = config.username;
-    var password = config.password;
-    doRest(username, password);
+// read from environment config if on heroku
+try {
+  var fileContents = fs.readFileSync('config.json');
+  var config = JSON.parse(fileContents);
+  var username = config.username;
+  var password = config.password;
+} catch(e) {
+  var username = process.env.USERNAME;
+  var password = process.env.PASSWORD;
+}
+
+var options = {
+  options: {
+    debug: true
+  },
+  connection: {
+    cluster: 'aws',
+    reconnect: true
+  },
+  identity: {
+    username,
+    password
+  },
+  //channels: ['arteezy', 'eternalenvyy']
+  channels: ['rome_bop']
+};
+
+var client = new tmi.client(options);
+client.connect();
+
+client.on('chat', function(channel, user, message, self) {
+  function record(target) {
+    if (user['display-name'] === target || user['display-name'] === 'rome_bop') {
+      var msg = { date: Date.now(), message }; 
+      storeJSON(target + '.json', msg);
+    }
+  }
+  record('Arteezy');
+  record('Eternalenvyy');
+  if (message === 'romo_boto are you there?') {
+    client.action(channel, "I am here! :D");
   }
 });
 
-function doRest(username, password) {
-  
-  var options = {
-    options: {
-      debug: true
-    },
-    connection: {
-      cluster: 'aws',
-      reconncet: true
-    },
-    identity: {
-      username,
-      password
-    },
-    //channels: ['arteezy', 'eternalenvyy']
-    channels: ['rome_bop']
-  };
-
-  var client = new tmi.client(options);
-  client.connect();
-
+// socket new chat messages to client
+io.on('connection', function(socket) {
   client.on('chat', function(channel, user, message, self) {
-    function record(target) {
+    function update(target) {
       if (user['display-name'] === target || user['display-name'] === 'rome_bop') {
-        var msg = { date: Date.now(), message }; 
-        storeJSON(target + '.json', msg);
+        var msg = { date: Date.now(), message, target }; 
+        socket.emit('message', msg);
       }
     }
-    record('Arteezy');
-    record('Eternalenvyy');
-    if (message === 'romo_boto are you there?') {
-      client.action(channel, "I am here! :D");
-    }
+    update('Arteezy');
+    update('Eternalenvyy');
   });
-
-  // socket new chat messages to client
-  io.on('connection', function(socket) {
-    client.on('chat', function(channel, user, message, self) {
-      function update(target) {
-        if (user['display-name'] === target || user['display-name'] === 'rome_bop') {
-          var msg = { date: Date.now(), message, target }; 
-          socket.emit('message', msg);
-        }
-      }
-      update('Arteezy');
-      update('Eternalenvyy');
-    });
-  });
-
-};
+});
 
 function storeJSON(file, message) {
   fs.readFile(file, function(err, data) {
